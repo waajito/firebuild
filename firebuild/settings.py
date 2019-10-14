577abb08cc75f73171759b9cf61e8ef3803be262
Bharadwaj <bharadwajpalakurthy@gmail.com>
2019-10-15 02:26:54 +0530

    make it better

--- firebuild/settings.py
+++ firebuild/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')r$lmu*=dk0c&denw%b182v1zg0-tr0uajhk(0$husbkj=^(4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
